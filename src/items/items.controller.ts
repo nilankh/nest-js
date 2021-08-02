53cf7534a7560c8ba31b84e0382fd8767641fc9f
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-02 12:22:46 +0530

    learned about deleting

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -30,4 +30,9 @@ export class ItemsController {
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
   }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
 }
