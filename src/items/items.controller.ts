577df9bf0b4938ad7ba95120453420de939e33ac
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-02 12:16:01 +0530

    Learned about param

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -5,18 +5,25 @@ import {
   Put,
   Delete,
   Body,
-  Req,
-  Res,
+  Param,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item-dto';
-import { Request, Response } from 'express';
 
 @Controller('items')
 export class ItemsController {
   @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    console.log(req.url);
-    return res.send('Hello World');
+  findAll(): string {
+    return 'Get all items';
+  }
+
+  //   @Get(':id')
+  //   findOne(@Param() param): string {
+  //     return `Item ${param.id}`;
+  //   }
+  //   Another way of doing above
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
   }
 
   @Post()
