bab8a2bf41879791613f57bc91df9740bb3fabea
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-02 12:10:58 +0530

    Learning routes

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item-dto';
+import { Request, Response } from 'express';
+
 @Controller('items')
 export class ItemsController {
-    @Get()
-    findAll(): string{
-        return 'Get all items';
-    }
+  @Get()
+  findAll(@Req() req: Request, @Res() res: Response): Response {
+    console.log(req.url);
+    return res.send('Hello World');
+  }
 
-    @Post()
-    create(@Body() createItemDto: CreateItemDto): string{
-        return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`
-    }
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
 }
