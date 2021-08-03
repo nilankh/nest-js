08316e6c2fd2afc74a2f66a2637b502920d94459
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-03 22:51:56 +0530

    Learning variable types

--- main1.ts
+++ main1.ts
@@ -1,9 +1,20 @@
-export {}
+export {};
 let isBeginner: boolean = true;
 let total: number = 0;
-let name: string= 'Nilank Nikhil';
+let name: string = 'Nilank Nikhil';
 
 let sentence: string = `My name is ${name}
 I am a beginner in Typescript`;
 
-console.log(sentence)
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+
+let person1: [string, number] = ['Nilank', 22];
