ebf36bed444a828c9e56034703708c77a0d7c454
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-03 22:35:34 +0530

    Learning variable types

---
+++ main1.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var isBeginner = true;
+var total = 0;
+var name = 'Nilank Nikhil';
+var sentence = "My name is " + name + "\nI am a beginner in Typescript";
+console.log(sentence);
---
+++ main1.ts
@@ -0,0 +1,9 @@
+export {}
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string= 'Nilank Nikhil';
+
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence)
