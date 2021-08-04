04839bc2728781780b7d486883603f96aa8e42e7
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-04 10:21:40 +0530

    classes and inheritance

--- main1.js
+++ main1.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var isBeginner = true;
 var total = 0;
@@ -18,7 +33,6 @@ var Color;
     Color[Color["Green"] = 1] = "Green";
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
-;
 var c = Color.Green;
 console.log(c);
 (function (Color) {
@@ -26,6 +40,97 @@ console.log(c);
     Color[Color["Black"] = 6] = "Black";
     Color[Color["Yellow"] = 7] = "Yellow";
 })(Color || (Color = {}));
-;
 var d = Color.Black;
 console.log(d);
+// Any type
+var randomValue = 10;
+randomValue = true;
+randomValue = 'Nilank';
+var myVariable = 10;
+// console.log(myVariable.name);
+// myVariable();
+// myVariable.toUpperCase();
+// let myVar: unknown = 10;
+// function hasName(obj: any): obj is { name: string}{
+//     return !!obj && typeof obj === "object" && "name" in obj
+// }
+// if(hasName(myVar)){
+//     console.log(myVar.name);
+// }
+// (myVar as string).toUpperCase();
+var a;
+a = 10;
+a = true;
+var b = 20;
+var multiType;
+multiType = 20;
+multiType = true;
+function add(num1, num2) {
+    return num1 + num2;
+}
+// If you want to keep optional to any value
+function add1(num1, num2) {
+    return num1 + num2;
+}
+function add2(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+function add3(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    return num1 + num2;
+}
+add3(5, 10);
+add3(20);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: 'Nilank',
+    lastName: 'Nikhil'
+};
+fullName(p);
+function fullNamee(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var q = {
+    firstName: 'Nilank',
+    lastName: 'Nikhil'
+};
+fullNamee(q);
+function fullNameee(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var r = {
+    firstName: 'Nilank'
+};
+fullNameee(r);
+// Classes & AccessModifiers
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.eName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good morning " + this.eName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Neel Punj');
+console.log(emp1.eName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log("Manager delegating task");
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Arpit');
+m1.delegateWork();
+m1.greet();
+console.log(m1.eName);
--- main1.ts
+++ main1.ts
@@ -19,14 +19,21 @@ let list2: Array<number> = [1, 2, 3];
 
 let person1: [string, number] = ['Nilank', 22];
 
-enum Color {Red, Green, Blue};
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
 let c: Color = Color.Green;
-console.log(c)
+console.log(c);
 
-
-enum Color {Pink = 5, Black, Yellow};
+enum Color {
+  Pink = 5,
+  Black,
+  Yellow,
+}
 let d: Color = Color.Black;
-console.log(d)
+console.log(d);
 
 // Any type
 let randomValue: any = 10;
@@ -34,16 +41,117 @@ randomValue = true;
 randomValue = 'Nilank';
 
 let myVariable: any = 10;
-console.log(myVariable.name);
-myVariable();
-myVariable.toUpperCase();
+// console.log(myVariable.name);
+// myVariable();
+// myVariable.toUpperCase();
 
-let myVar: unknown = 10;
-function hasName(obj: any): obj is { name: string}{
-    return !!obj && typeof obj === "object" && "name" in obj
+// let myVar: unknown = 10;
+// function hasName(obj: any): obj is { name: string}{
+//     return !!obj && typeof obj === "object" && "name" in obj
+// }
+
+// if(hasName(myVar)){
+//     console.log(myVar.name);
+// }
+// (myVar as string).toUpperCase();
+
+let a;
+a = 10;
+a = true;
+
+let b = 20;
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+function add(num1: number, num2: number): number {
+  return num1 + num2;
 }
 
-if(hasName(myVar)){
-    console.log(myVar.name);
+// If you want to keep optional to any value
+function add1(num1: number, num2?: number): number {
+  return num1 + num2;
 }
-(myVar as string).toUpperCase();
+
+function add2(num1: number, num2?: number): number {
+  if (num2) return num1 + num2;
+  else return num1;
+}
+
+function add3(num1: number, num2: number = 10): number {
+  return num1 + num2;
+}
+add3(5, 10);
+add3(20);
+
+function fullName(person: { firstName: string; lastName: string }) {
+  console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let p = {
+  firstName: 'Nilank',
+  lastName: 'Nikhil',
+};
+
+fullName(p);
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+function fullNamee(person: Person) {
+  console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let q = {
+  firstName: 'Nilank',
+  lastName: 'Nikhil',
+};
+
+fullNamee(q);
+
+// properties can be optional too
+interface Personn {
+  firstName: string;
+  lastName?: string;
+}
+function fullNameee(person: Personn) {
+  console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let r = {
+  firstName: 'Nilank',
+};
+
+fullNameee(r);
+
+
+// Classes & AccessModifiers
+class Employee{
+    eName: string;
+    constructor(name: string){
+        this.eName = name;
+    }
+    greet(){
+        console.log(`Good morning ${this.eName}`);
+    }
+}
+let emp1 = new Employee('Neel Punj');
+console.log(emp1.eName);
+emp1.greet();
+
+class Manager extends Employee{
+    constructor(managerName: string){
+        super(managerName);
+    }
+
+    delegateWork(){
+        console.log(`Manager delegating task`);
+    }
+}
+
+let m1 = new Manager('Arpit');
+m1.delegateWork();
+m1.greet();
+console.log(m1.eName);
