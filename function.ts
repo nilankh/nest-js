87db4d37e311ca59530c0fb333da65a9d52e8879
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-19 10:46:42 +0530

    revising typescript functions

--- function.ts
+++ function.ts
@@ -1,5 +1,16 @@
-function getSum(num1, num2){
+function getSum(num1: number, num2: number): number{
     return num1 + num2;
 }
 
-console.log(getSum(1,4))
+// console.log(getSum(1,4))
+
+let mySum = function (num1: number, num2: any) {
+  if(typeof num1 == 'string'){
+      num1 = parseInt(num1)
+  }
+  if(typeof num2 == 'string'){
+      num2 = parseInt(num2)
+  }
+    return num1 + num2;
+};
+console.log(mySum(1,"9"))
