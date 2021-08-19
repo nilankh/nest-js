0f2a0b0323aa69e75d2edd6f15084a8b9018c01d
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-19 10:53:48 +0530

    revising typescript fInterfaces

--- interface.js
+++ interface.js
@@ -1,3 +1,11 @@
+/*
+function showToDo(todo: {title: string, text: string}){
+    console.log(todo.title + ':' + todo.text)
+}
+let myTodo = {title: 'Trash' , text: 'Take out trash'}
+showToDo(myTodo)
+
+*/
 function showToDo(todo) {
     console.log(todo.title + ':' + todo.text);
 }
