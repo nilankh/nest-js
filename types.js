25c08a622163a756e5fdcb59c2ea49dee5c7dfc1
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-19 10:38:44 +0530

    revising typescript

--- types.js
+++ types.js
@@ -4,9 +4,15 @@
     var myNum = void 0;
     var myBool = void 0;
     var myVar = void 0;
+    /*
+    let strArr: string[];
+    let numArr: number[];
+    let boolArr: boolean[];
+    */
     var strArr = void 0;
     var numArr = void 0;
     var boolArr = void 0;
+    var strNumTuple = void 0;
     myString = 'Hello'.slice(0, 3);
     myNum = 1;
     myBool = false;
@@ -14,6 +20,6 @@
     strArr = ['hello', 'world'];
     numArr = [1, 2, 3];
     boolArr = [true, false, true];
-    // myString = "Hello World";
-    console.log(boolArr);
+    strNumTuple = ['Hello', 4];
+    console.log(strNumTuple);
 }
--- types.ts
+++ types.ts
@@ -5,9 +5,17 @@ let myNum: number;
 let myBool: boolean;
 let myVar: any;
 
+/*
 let strArr: string[];
 let numArr: number[];
 let boolArr: boolean[];
+*/
+
+let strArr: Array<string>;
+let numArr: Array<number>;
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number]
 
 myString = 'Hello'.slice(0,3);
 myNum = 1;
@@ -18,6 +26,7 @@ strArr = ['hello','world'];
 numArr = [1,2,3];
 boolArr = [true, false, true];
 
-// myString = "Hello World";
-console.log(boolArr);
+strNumTuple = ['Hello',4]
+
+console.log(strNumTuple);
 }
