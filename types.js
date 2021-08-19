28d4ba870ab309b4a83804e7de3b5e6982dd27d1
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-08-19 10:36:06 +0530

    revising typescript

---
+++ types.js
@@ -0,0 +1,19 @@
+// console.log('Hello from Ts')
+{
+    var myString = void 0;
+    var myNum = void 0;
+    var myBool = void 0;
+    var myVar = void 0;
+    var strArr = void 0;
+    var numArr = void 0;
+    var boolArr = void 0;
+    myString = 'Hello'.slice(0, 3);
+    myNum = 1;
+    myBool = false;
+    myVar = true;
+    strArr = ['hello', 'world'];
+    numArr = [1, 2, 3];
+    boolArr = [true, false, true];
+    // myString = "Hello World";
+    console.log(boolArr);
+}
---
+++ types.ts
@@ -0,0 +1,23 @@
+// console.log('Hello from Ts')
+{
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+
+myString = 'Hello'.slice(0,3);
+myNum = 1;
+myBool = false;
+myVar = true;
+
+strArr = ['hello','world'];
+numArr = [1,2,3];
+boolArr = [true, false, true];
+
+// myString = "Hello World";
+console.log(boolArr);
+}
